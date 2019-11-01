c389e480df7566a7c32d41b380cc196c9d33550c
Minna Niemi <minna.niemi@futurice.com>
2019-11-01 23:30:01 +0000

    Add a type declaration file

---
+++ index.d.ts
@@ -0,0 +1,42 @@
+import React from "react";
+
+declare module "rn-range-slider" {
+  interface RangeSliderProps {
+    rangeEnabled: boolean;
+    disabled: boolean;
+    valueType: "number" | "time";
+    gravity: "top" | "bottom" | "center";
+    min: number | Date;
+    max: number | Date;
+    step: number | Date;
+    initialLowValue: number | Date;
+    initialHighValue: number | Date;
+    lineWidth: number;
+    thumbRadius: number;
+    thumbBorderWidth: number;
+    labelStyle: "none" | "bubble";
+    labelGapHeight: number;
+    labelTailHeight: number;
+    labelFontSize: number;
+    labelBorderWidth: number;
+    labelPadding: number;
+    labelBorderRadius: number;
+    textFormat: string;
+    blankColor: string;
+    selectionColor: string;
+    thumbColor: string;
+    thumbBorderColor: string;
+    labelTextColor: string;
+    labelBackgroundColor: string;
+    labelBorderColor: string;
+    onTouchStart: () => void;
+    onTouchEnd: () => void;
+    onValueChanged: (
+      lowValue: number | Date,
+      highValue: number | Date,
+      fromUser: boolean
+    ) => void;
+  }
+
+  export default class RangeSlider extends React.Component<RangeSliderProps> {}
+}
